1dc4a39e3a6327e0dc3d57c916b7dfa92ad9814d
Afif <afifashroff@gmail.com>
2020-08-23 07:20:36 +0800

    fix(shared-interfaces): Add contactdetails into PostingPayload

--- shared-interfaces.ts
+++ shared-interfaces.ts
@@ -128,6 +128,8 @@ export interface PostingPayload {
     logoid: string
     /** File ID */
     bgimgid: string
+    /** Same as Contact.contactdetails */
+    contactdetails?: ContactDetail[]
 }
 
 export interface FileDB {
